28b950ee4bba95eb7f6002b890dc24d7a0467494
Chris <electronicsleep@gmail.com>
2022-07-03 09:57:59 -0700

    update-rust-2021

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-#export datasource_conn_string=mysql://infradb:password@localhost:3306/infradb
-export DATABASE_URL=mysql://infradb:password@localhost:3306/infradb
+export datasource_conn_string=mysql://infradb:password@localhost:3306/infradb
 cargo fmt
 cargo build --release
 ./target/release/rust_service
