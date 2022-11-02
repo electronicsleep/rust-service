7e88ae3280c0ea47b3a2ad17c6b9a059f42f37a0
Chris <electronicsleep@gmail.com>
2022-11-01 20:25:49 -0700

    optional-datetime-updates

--- src/test/curl-tests.sh
+++ src/test/curl-tests.sh
@@ -11,11 +11,9 @@ curl --fail -X GET http://0.0.0.0:8080/home
 echo "-"
 curl --fail -X GET http://0.0.0.0:8080/about
 echo "-"
-curl --fail -X POST http://0.0.0.0:8080/echo -H 'Content-Type: application/json' -d '{"service":"infra-service","event":"deploy-infrasvc-v0.0.1", "event_type":"deploy-qa"}'
+curl --fail -X POST http://0.0.0.0:8080/add -H 'Content-Type: application/json' -d '{"service":"infrasvc","event":"deploy-infrasvc-v0.0.3", "event_type":"deploy-qa"}'
 echo "-"
-curl --fail -X POST http://0.0.0.0:8080/add -H 'Content-Type: application/json' -d '{"service":"infra-service","event":"deploy-infrasvc-v0.0.1", "event_type":"deploy-qa"}'
-echo "-"
-curl --fail -X POST http://0.0.0.0:8080/add -H 'Content-Type: application/json' -d '{"service":"infrasvc","event":"deploy-infrasvc-v0.0.2", "event_type":"deploy-qa"}'
+curl --fail -X POST http://0.0.0.0:8080/add -H 'Content-Type: application/json' -d '{"service":"infrasvc","event":"deploy-infrasvc-v0.0.3", "event_type":"deploy-qa", "datetime": "2022-11-01 00:00:00"}'
 echo "-"
 curl --fail -I -X GET http://0.0.0.0:8080/events
 echo -e "\nTests Pass"
